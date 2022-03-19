7472e8a75332f2b9392dad060e7e267457f3315b
sudochu <2086355155@qq.com>
2022-03-19 14:22:18 +0800

    third chars test

--- edu.chu/chars/demo.java
+++ edu.chu/chars/demo.java
@@ -3,23 +3,23 @@ package chars;
 import java.util.HashMap;
 import java.util.Map;
 
-    public class demo {
+public class demo {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            String str = "!adhanlf@%nlesngal&*gnksvks esfkefbmflbskgbskbsf@lnefnwel";
+        String str = "!adhanlf@%nlesngal&*gnksvks esfkefbmflbskgbskbsf@lnefnwel";
 
-            char[] chars = str.toCharArray();
+        char[] chars = str.toCharArray();
 
-            Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
 
-            for(char c : chars){
-                if(map.containsKey(c))map.put(c,map.get(c)+1);
-                else map.put(c,1);
-            }
-
-            for(Map.Entry<Character,Integer> e: map.entrySet())
-                System.out.println(e.getKey()+" "+e.getValue());
-
+        for(char c : chars){
+            if(map.containsKey(c))map.put(c,map.get(c)+1);
+            else map.put(c,1);
         }
+
+        for(Map.Entry<Character,Integer> e: map.entrySet())
+            System.out.println(e.getKey()+" "+e.getValue());
+
     }
+}
