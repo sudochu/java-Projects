2ffae0794bd5d8ba0eae67eb53720e605785aadc
lemonChu <65015522+sudochu@users.noreply.github.com>
2022-03-19 16:32:16 +0800

    update chars project.
    advanced version

--- edu.chu/chars/demo.java
+++ edu.chu/chars/demo.java
@@ -1,18 +1,17 @@
 package chars;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class demo {
 
     public static void main(String[] args) {
 
-        String str = "!adhanlf@%nlesngal&*gnksvks esfkefbmflbskgbskbsf@lnefnwel";
+        String str = "!adhanlf@%nleKBSgal&*gnksvks esKDhGKfbm*flbskgbskbsf@lnefnwel";//disordered string
 
         char[] chars = str.toCharArray();
 
-        Map<Character, Integer> map = new HashMap<>();
-
+        TreeMap<Character, Integer> map = new TreeMap<>();
         for(char c : chars){
             if(map.containsKey(c))map.put(c,map.get(c)+1);
             else map.put(c,1);
