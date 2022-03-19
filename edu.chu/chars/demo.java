cc906f3b1a7a0905e23fd243379ed35b91a32d21
sudochu <2086355155@qq.com>
2022-03-19 14:14:09 +0800

    first chars test

---
+++ edu.chu/chars/demo.java
@@ -0,0 +1,25 @@
+package chars;
+
+import java.util.HashMap;
+import java.util.Map;
+
+    public class demo {
+
+        public static void main(String[] args) {
+
+            String str = new String("!adhanlf@%nlesngal&*gnksvks esfkefbmflbskgbskbsf@lnefnwel");
+
+            char[] chars = str.toCharArray();
+
+            Map<Character, Integer> map = new HashMap<>();
+
+            for(char c : chars){
+                if(map.containsKey(c))map.put(c,map.get(c)+1);
+                else map.put(c,1);
+            }
+
+            for(Map.Entry e: map.entrySet())
+                System.out.println(e.getKey()+" "+e.getValue());
+
+        }
+    }
